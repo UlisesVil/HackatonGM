4eb28433ba1b732f9da459c349f8089c4354b028
jhoelssf <jhoelssflorez@gmail.com>
2021-09-26 13:12:09 -0500

    list with mock

--- src/app/news-feed/item/item.component.ts
+++ src/app/news-feed/item/item.component.ts
@@ -1,6 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {PasswordValidation} from '../../auth/utils/matchPassword';
 import {Bootcamp} from '../../models/bootcamp';
 
 @Component({
